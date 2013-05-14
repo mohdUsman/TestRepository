d028b9763fb0915f156a6e3ba6cb64864f5d81b2
Muhammad Usman <Mani@Apples-MacBook.local>
2013-05-14 11:32:31 +0500

    Initial Commit

---
+++ BongoSpellingAdventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BongoSpellingAdventure
+//
+//  Created by Muhammad Usman on 5/14/13.
+//  Copyright (c) 2013 Muhammad Usman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BongoSpellingAdventure/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BongoSpellingAdventure
+//
+//  Created by Muhammad Usman on 5/14/13.
+//  Copyright (c) 2013 Muhammad Usman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
