e0eba8fe4df50475cdfe3adbe480e29b306cc582
Wajiha Kanwal <wajiha.kanwal@talismaworks.com>
2013-05-17 16:15:49 +0500

    MoreApps model class added

---
+++ BongoSpellingAdventure/Classes/MoreApps.h
@@ -0,0 +1,19 @@
+//
+//  MoreGame.h
+//  BongoSpellingAdventure
+//
+//  Created by Wajiha Kanwal on 5/17/13.
+//  Copyright (c) 2013 TalismaWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MoreApps : NSObject {
+    
+    NSString* appName;
+    //icon
+}
+
+@property (strong, nonatomic) NSString* appName;
+
+@end
---
+++ BongoSpellingAdventure/Classes/MoreApps.m
@@ -0,0 +1,15 @@
+//
+//  MoreGame.m
+//  BongoSpellingAdventure
+//
+//  Created by Wajiha Kanwal on 5/17/13.
+//  Copyright (c) 2013 TalismaWorks. All rights reserved.
+//
+
+#import "MoreApps.h"
+
+@implementation MoreApps
+
+@synthesize appName;
+
+@end
